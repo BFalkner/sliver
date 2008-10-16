35485f3e009d1c7210843eb6af9c1d5155114841
Brennan Falkner <fishy5@gmail.com>
2008-10-16 18:35:39 -0400

    Add some parsing tests and corresponding element cases.

--- Parse/Element.fs
+++ Parse/Element.fs
@@ -2,3 +2,7 @@
 
 type element =
     | Text of string
+    | Anchor of string * string
+    | OrderedList of element list
+    | UnorderedList of element list
+    | Component of unit
--- Test/Parser.fs
+++ Test/Parser.fs
@@ -4,6 +4,29 @@ open Xunit
 open Lexing
 open Element
 
+let Parse s =
+    Parser.template Lexer.lexer (Lexing.from_string s)
+
 [<Fact>]
-let TextBlock() =
-    Assert.Equal([Text("Test")], Parser.template Lexer.lexer (Lexing.from_string "Test"))
+let TextTest() =
+    Assert.Equal([Text("Test")], Parse "Test")
+
+[<Fact>]
+let AnchorTest() =
+    Assert.Equal([Anchor("http://google.com/", "http://google.com/")], Parse "<http://google.com/>")
+    
+[<Fact>]
+let AnchorWithTextTest() =
+    Assert.Equal([Anchor("Google Search Engine", "http://google.com/")], Parse "\"Google Search Engine\" <http://google.com/>")
+
+[<Fact>]
+let OrderedListTest() =
+    Assert.Equal([OrderedList([Text("Blah"); Text("Meh")])], Parse "# Blah\n# Meh")
+    
+[<Fact>]
+let UnorderedListTest() =
+    Assert.Equal([UnorderedList([Text("Blah"); Text("Meh")])], Parse "* Blah\n* Meh")
+
+[<Fact>]
+let ComponentTest() =
+    Assert.Equal([Component()], Parse "[post:blah]")
